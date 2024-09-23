c1744a4afa22d63b4602e31fdaa005d6e1bcecb0
cdev2024 <creativedev2024@gmail.com>
2024-09-23 11:31:20 +0900

    [20240923] pm11:30

---
+++ src/main/java/com/example/mywebservice/dto/AddUserDto.java
@@ -0,0 +1,11 @@
+package com.example.mywebservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddUserDto {
+    private String email;
+    private String password;
+}
